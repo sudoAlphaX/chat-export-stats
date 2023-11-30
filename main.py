5d6fff941401736a47136c0a0e106c3774af653c
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2023-11-30 21:41:35 +0530

    remove legacy instagram chat process

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-import json
-import string
+from handlers import instagram
 
-with open("test/message_1.json", "r") as f:
-    data = json.load(f)
-
-# print(data.keys())
-
-wordlist = {}
-translator = str.maketrans("", "", string.punctuation)
-
-# msg = (data["messages"][3]["content"]).translate(translator).split()
-
-
-for i in data["messages"]:
-    if i.get("content", False):
-        words = (i["content"]).translate(translator).lower().split()
-
-        for j in words:
-            if j in wordlist:
-                wordlist[j] += 1
-            else:
-                wordlist[j] = 1
-
-sorted_dict = dict(sorted(wordlist.items(), key=lambda x: x[1], reverse=True))
-print(sorted_dict)
-
+a = instagram.read_chat_export()
