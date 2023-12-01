f9b8cb674f8b6ef669633ba8acba2a6ad4a517f3
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2023-12-01 09:54:59 +0000

    add function to get word count from each participant

--- handlers/instagram.py
+++ handlers/instagram.py
@@ -1,4 +1,5 @@
 import json
+import string
 
 
 def read_chat_export(filename="message_1.json"):
@@ -38,3 +39,31 @@ def read_chat_export(filename="message_1.json"):
     data["messages"].reverse()
 
     return data
+
+
+def get_words_count(rawdata):
+    wordlist = {}
+    translator = str.maketrans("", "", string.punctuation)
+
+    pdict = {p["name"]: 0 for p in rawdata["participants"]}
+
+    for msg in rawdata["messages"]:
+        if msg.get("type") == "text":
+            words = msg["content"].translate(translator).lower().split()
+            for word in words:
+                if word not in wordlist:
+                    wordlist[word] = {"total": 0, **pdict}
+                wordlist[word]["total"] += 1
+                wordlist[word][msg["sender_name"]] += 1
+
+    sorted_wordlist = dict(
+        sorted(wordlist.items(), key=lambda x: x[1]["total"], reverse=True)
+    )
+
+    return sorted_wordlist
+
+
+if __name__ == "__main__":
+    data = read_chat_export()
+    wordcount = get_words_count(data)
+    print(wordcount)
